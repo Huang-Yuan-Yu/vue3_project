db146199924107d7b8749a2b5ba22e71172d7c6e
黄元昱 <2690085099@qq.com>
2022-11-16 10:41:45 +0800

    优化兼容性

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,12 @@ module.exports = {
                 ],
             },
         ],
+        [
+            "@babel/preset-env",
+            {
+                targets: "> 0.25%, not dead",
+            },
+        ],
     ],
     // 下面这行是JSX需要配置的，以及分包打包
     plugins: ["@vue/babel-plugin-jsx", "@babel/plugin-syntax-dynamic-import"],
