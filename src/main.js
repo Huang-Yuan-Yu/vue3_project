008fedc9be88d80e02380256ecee298efe51eade
黄元昱 <2690085099@qq.com>
2022-11-25 16:31:13 +0800

    增加页面加载进度条

--- src/main.js
+++ src/main.js
@@ -20,8 +20,29 @@ import axios from "axios";
 // 提高滚动性能——npm i default-passive-events -S
 import "default-passive-events";
 
+// 引入nprogress，页面加载进度条——npm install --save nprogress
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 // 视图
 import App from "@/views/App";
+
+NProgress.configure({
+    easing: "ease", // 动画方式
+    speed: 500, // 递增进度条的速度
+    showSpinner: false, // 是否显示加载ico
+    trickleSpeed: 200, // 自动递增间隔
+    minimum: 0.3, // 初始化时的最小百分比
+});
+router.beforeEach((to, from, next) => {
+    // 每次切换页面时，调用进度条
+    NProgress.start();
+    // 这个一定要加，没有next()页面不会跳转的。这部分还不清楚的去翻一下官网就明白了
+    next();
+});
+router.afterEach(() => {
+    // 在即将进入新的页面组件前，关闭掉进度条
+    NProgress.done();
+});
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
